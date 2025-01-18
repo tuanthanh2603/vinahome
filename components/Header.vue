81f1c4c095c4e849afd0b9f1f084a9e0a9197ec1
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2025-01-18 17:45:18 +0700

    init header

---
+++ components/Header.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+// Script setup for future interactivity if needed
+import {Headset, ArrowDown} from "@element-plus/icons-vue";
+</script>
+
+<template>
+  <nav class="bg-[#0072bc] shadow-md h-16 w-full">
+    <div class="container mx-auto flex items-center justify-between px-4 h-full">
+    <!-- Logo Section -->
+    <div class="flex items-center">
+      <img
+          src="/static/logo-2.png"
+          alt="Logo"
+          class="w-40"
+      />
+    </div>
+      <ul class="flex space-x-2">
+        <li>
+          <el-button text class="button-link">Tra cứu vé</el-button>
+        </li>
+        <li>
+          <el-button text class="button-link">Mở bán vé trên VinaHome</el-button>
+        </li>
+        <li>
+          <el-dropdown>
+            <el-button text class="button-link">
+              Trở thành đối tác<el-icon class="el-icon--right"><ArrowDown /></el-icon>
+            </el-button>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item>Phần mềm quản lý nhà xe</el-dropdown-item>
+                <el-dropdown-item>Phần mềm quản lý bán vé</el-dropdown-item>
+                <el-dropdown-item>Phần mềm quản lý hàng hoá</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </li>
+        <li>
+          <el-button type="warning" :icon="Headset" plain>Hỗ trợ 24/7</el-button>
+        </li>
+        <li>
+          <el-button type="primary" plain>Đăng nhập</el-button>
+        </li>
+
+
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.button-link{
+  color: white;
+
+}
+.button-link:hover {
+  color: black;
+}
+.el-button:focus-visible, .el-button.is-text:not(.is-disabled):focus-visible{
+  outline: none;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  ssr: true,
+  css: ['~/assets/css/main.css', '~/assets/scss/main.scss'],
+  modules: [
+    '@nuxtjs/tailwindcss', '@pinia/nuxt',
+  ],
+
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_URL || 'http://localhost:8080',
+    },
+  },
+
+
+  app: {
+    head: {
+      title: 'VinaHome',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        {
+          name: "description",
+          content: "VinaHome"
+        },
+        {
+          name: "og:title",
+          content: "VinaHome"
+        },
+        {
+          name: "og:description",
+          content: "VinaHome"
+        },
+        {
+          name: "og:site_name",
+          content: "VinaHome"
+        },
+      ]
+    }
+  }
+});
