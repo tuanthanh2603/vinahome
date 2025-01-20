8683985a3a4d13391083b060155ca59c30ef71f9
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2025-01-20 10:00:59 +0700

    init

---
+++ components/SelectTrip.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import {Headset, MapLocation} from "@element-plus/icons-vue";
+</script>
+
+<template>
+  <!-- Bọc cả phần tử container và nút vào một div flex -->
+  <div class="flex items-center px-5 py-2">
+    <!-- Container chính với chiều cao h-20 -->
+    <div class="h-16 border border-gray-200 rounded-lg flex items-center flex-1 ">
+      <!-- Phần tử bên trái -->
+      <div class="flex-1 border-r border-gray-200 flex justify-between w-[200px] items-center m-4">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-5 w-5 mr-2 icon-pointUp"><path d="M448 256A192 192 0 1 0 64 256a192 192 0 1 0 384 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm0-224a144 144 0 1 1 0 288 144 144 0 1 1 0-288zM224 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
+        <div class="flex flex-col w-full m-2">
+          <span class="text-[13px] text-gray-400">Nơi đi</span>
+          <input type="text" class="w-full  rounded-md bg-transparent focus:outline-none"/>
+        </div>
+      </div>
+
+      <div class="flex-1 border-r border-gray-200 flex justify-between w-[200px] items-center">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="h-5 w-5 mr-2 icon-pointDown"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+        <div class="flex flex-col w-full m-2">
+          <span class="text-[13px] text-gray-400">Nơi đến</span>
+          <input type="text" class="w-full  rounded-md bg-transparent focus:outline-none"/>
+        </div>
+      </div>
+      <div class="flex-1  flex justify-between w-[200px] items-center m-4">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-5 w-5 mr-2 icon-calendar"><path d="M128 0c17.7 0 32 14.3 32 32l0 32 128 0 0-32c0-17.7 14.3-32 32-32s32 14.3 32 32l0 32 48 0c26.5 0 48 21.5 48 48l0 48L0 160l0-48C0 85.5 21.5 64 48 64l48 0 0-32c0-17.7 14.3-32 32-32zM0 192l448 0 0 272c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 192zm64 80l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm128 0l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zM64 400l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zm112 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16z"/></svg>
+        <div class="flex flex-col w-full m-2">
+          <span class="text-[13px] text-gray-400">Ngày đi</span>
+          <input type="text" class="w-full  rounded-md bg-transparent focus:outline-none"/>
+        </div>
+      </div>
+    </div>
+
+
+    <button class="bg-yellow-500 hover:bg-yellow-600 text-white rounded-lg ml-5 h-14 w-[150px]">
+      Tìm kiếm
+    </button>
+
+
+  </div>
+</template>
+
+
+
+
+
+
+<style scoped>
+.icon-calendar{
+  fill: #0072bc;
+}
+.icon-pointDown{
+  fill: #F04770;
+}
+.icon-pointUp{
+  fill: #108AB1;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,45 @@
 <script setup lang="ts">
-
-import SelectService from "~/components/SelectService.vue";
+import {MapLocation} from "@element-plus/icons-vue";
+import SelectTrip from "~/components/SelectTrip.vue";
 </script>
 
 <template>
-  <SelectService/>
-<img src="https://s3.rdbuz.com/web/images/vnm/vnmBannerNew.jpg"/>
+  <section class="flex justify-center items-center">
+    <div class="bg-[#ff2] custom-tabs rounded-lg py-3">
+      <el-tabs class="">
+        <el-tab-pane>
+          <template #label>
+            <span class="custom-tabs-label flex items-center space-x-2 mb-3">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="currentColor" class="w-4 h-4"><path d="M288 0C422.4 0 512 35.2 512 80l0 16 0 32c17.7 0 32 14.3 32 32l0 64c0 17.7-14.3 32-32 32l0 160c0 17.7-14.3 32-32 32l0 32c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-32-192 0 0 32c0 17.7-14.3 32-32 32l-32 0c-17.7 0-32-14.3-32-32l0-32c-17.7 0-32-14.3-32-32l0-160c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32c0 0 0 0 0 0l0-32s0 0 0 0l0-16C64 35.2 153.6 0 288 0zM128 160l0 96c0 17.7 14.3 32 32 32l112 0 0-160-112 0c-17.7 0-32 14.3-32 32zM304 288l112 0c17.7 0 32-14.3 32-32l0-96c0-17.7-14.3-32-32-32l-112 0 0 160zM144 400a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm288 0a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM384 80c0-8.8-7.2-16-16-16L208 64c-8.8 0-16 7.2-16 16s7.2 16 16 16l160 0c8.8 0 16-7.2 16-16z"/></svg>
+              <span class="text-[17px]">Xe khách</span>
+            </span>
+          </template>
+          <SelectTrip/>
+        </el-tab-pane>
+        <el-tab-pane>
+          <template #label>
+            <span class="custom-tabs-label flex items-center space-x-2 mb-3">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" fill="currentColor" class="w-4 h-4"><path d="M312 32c-13.3 0-24 10.7-24 24s10.7 24 24 24l25.7 0 34.6 64-149.4 0-27.4-38C191 99.7 183.7 96 176 96l-56 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l43.7 0 22.1 30.7-26.6 53.1c-10-2.5-20.5-3.8-31.2-3.8C57.3 224 0 281.3 0 352s57.3 128 128 128c65.3 0 119.1-48.9 127-112l49 0c8.5 0 16.3-4.5 20.7-11.8l84.8-143.5 21.7 40.1C402.4 276.3 384 312 384 352c0 70.7 57.3 128 128 128s128-57.3 128-128s-57.3-128-128-128c-13.5 0-26.5 2.1-38.7 6L375.4 48.8C369.8 38.4 359 32 347.2 32L312 32zM458.6 303.7l32.3 59.7c6.3 11.7 20.9 16 32.5 9.7s16-20.9 9.7-32.5l-32.3-59.7c3.6-.6 7.4-.9 11.2-.9c39.8 0 72 32.2 72 72s-32.2 72-72 72s-72-32.2-72-72c0-18.6 7-35.5 18.6-48.3zM133.2 368l65 0c-7.3 32.1-36 56-70.2 56c-39.8 0-72-32.2-72-72s32.2-72 72-72c1.7 0 3.4 .1 5.1 .2l-24.2 48.5c-9 18.1 4.1 39.4 24.3 39.4zm33.7-48l50.7-101.3 72.9 101.2-.1 .1-123.5 0zm90.6-128l108.5 0L317 274.8 257.4 192z"/></svg>
+              <span class="text-[17px]">Thuê xe</span>
+            </span>
+          </template>
+        </el-tab-pane>
+        <el-tab-pane >
+          <template #label>
+            <span class="custom-tabs-label flex items-center space-x-2 mb-3">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" fill="currentColor" class="w-4 h-4"><path d="M48 0C21.5 0 0 21.5 0 48L0 464c0 26.5 21.5 48 48 48l96 0 0-80c0-26.5 21.5-48 48-48s48 21.5 48 48l0 80 96 0c26.5 0 48-21.5 48-48l0-416c0-26.5-21.5-48-48-48L48 0zM64 240c0-8.8 7.2-16 16-16l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32zm112-16l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32zM80 96l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32zM272 96l32 0c8.8 0 16 7.2 16 16l0 32c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-32c0-8.8 7.2-16 16-16z"/></svg>
+              <span class="text-[17px]">Khách sạn</span>
+            </span>
+          </template>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </section>
+
 </template>
 
 <style scoped>
-
+.custom-tabs-label svg{
+  width: 20px;
+}
 </style>
